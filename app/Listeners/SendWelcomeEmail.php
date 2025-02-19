1a491c38bd828e1f55fd46f11f00f98dc7bfd48d
IIPN Solutions <guptanishantmca@gmail.com>
2025-02-19 11:34:32 +0530

    Update SendWelcomeEmail.php

--- app/Listeners/SendWelcomeEmail.php
+++ app/Listeners/SendWelcomeEmail.php
@@ -7,17 +7,17 @@ use Illuminate\Support\Facades\Mail;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use App\Mail\Auth\WelcomeEmail;
 
- 
 
-class SendWelcomeEmail  
+
+class SendWelcomeEmail
 {
-     
+
     /**
      * Handle the event.
      */
     public function handle(UserRegistered $event): void
     {
-        
+
         try {
             Mail::to($event->user->email)->send(new WelcomeEmail($event->user));
             Log::info('Email successfully sent to: ' . $event->user->email);
